2d8cbcf5b83249076ceb33e1a46569797b4876fa
nattamon19 <ploy19072004@gmail.com>
2023-03-28 12:25:49 +0700

    exception1

---
+++ src/exception1/Lab3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception1;
+
+/**
+ *
+ * @author Student
+ */
+public class Lab3 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("statement 1");
+            System.out.println("statement 2");
+            System.out.println("statement 3");
+        }
+        catch(Exception e) {
+        }
+        finally {
+            System.out.println("statement 4");
+        }
+        System.out.println("statement 5");
+    }
+}
